b166bad4c3a8db4f3f30a77f0afb415e77b6c0d6
Hardik Patel <hpatel@vavni.com>
2014-08-18 14:00:14 +0530

    Added foreign key field in testcase database

--- TCMS/testcase/models.py
+++ TCMS/testcase/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class TestCase(django_models.Model):
     project = django_models.ForeignKey(project_models.Project)
+    category = django_models.ForeignKey(project_models.Category)
     title = django_models.CharField(max_length=200)
     steps = django_models.TextField()
     expected_result = django_models.TextField()
